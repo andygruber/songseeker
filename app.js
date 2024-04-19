f179aa9567a0f526f68d805802e4060238d98f22
Andreas Gruber <andreas@gruber.info>
2024-04-19 19:08:22 +0200

    Test

--- app.js
+++ app.js
@@ -6,6 +6,78 @@ let playbackDuration = 30; // Default playback duration
 let qrScanner;
 let csvCache = {};
 
+const generateRandomString = (length) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
+const sha256 = async (plain) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+}
+
+const base64encode = (input) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+      .replace(/=/g, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+}
+  
+const codeVerifier  = generateRandomString(64);
+const hashed = await sha256(codeVerifier)
+const codeChallenge = base64encode(hashed);
+
+const spotifyClientId = 'f517c8b4e0ef49d1877bd9eaf2d82a5e';
+const spotifyRedirectUri = window.location.origin;
+
+const spotifyScope = 'user-read-private user-read-email';
+const spotifyAuthUrl = new URL("https://accounts.spotify.com/authorize")
+
+// generated in the previous step
+window.localStorage.setItem('code_verifier', codeVerifier);
+
+const params =  {
+  response_type: 'code',
+  client_id: spotifyClientId,
+  spotifyScope,
+  code_challenge_method: 'S256',
+  code_challenge: codeChallenge,
+  redirect_uri: spotifyRedirectUri,
+}
+
+spotifyAuthUrl.search = new URLSearchParams(params).toString();
+window.location.href = spotifyAuthUrl.toString();
+
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+const getToken = async code => {
+
+    // stored in the previous step
+    let codeVerifier = localStorage.getItem('code_verifier');
+  
+    const payload = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
+      }),
+    }
+  
+    const body = await fetch(url, payload);
+    const response =await body.json();
+  
+    localStorage.setItem('access_token', response.access_token);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
 
     let lastDecodedText = ""; // Store the last decoded text
